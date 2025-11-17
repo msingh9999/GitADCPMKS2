4ed2a494139dfcfeea335136e0af4fb764d81705
Manoj Singh <manoj_singh@bmc.com>
2025-11-17 09:53:06 -0600

    11-17-25-001

--- COB/CWXTSUBC.cbl
+++ COB/CWXTSUBC.cbl
@@ -6,7 +6,7 @@
       *                                                                *
       *  THIS CALLED PROGRAM IS THE COBOL DEMO PROGRAM USED FOR        *
       *  XPEDITER/TSO TRAINING SESSIONS.MKS                            *
-      *  Updated comment - 11/10/25 - 001                              *
+      *  Updated comment - 11/17/25 - 001                              *
       *                                                                *
       ******************************************************************
        ENVIRONMENT DIVISION.
